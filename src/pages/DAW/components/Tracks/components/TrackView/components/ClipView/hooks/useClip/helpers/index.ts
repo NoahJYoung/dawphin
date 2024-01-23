8024eb9514b99e6d30202a880c5e3f71e6b65d7d
Noah Young <noah.young@sqa-consulting.com>
2024-01-23 13:33:04 +0100

    moves drag handler creation to own file

---
+++ src/pages/DAW/components/Tracks/components/TrackView/components/ClipView/hooks/useClip/helpers/index.ts
@@ -0,0 +1,1 @@
+export { getDragHandler } from "./getDragHandler";
--- src/pages/DAW/components/Tracks/components/TrackView/components/ClipView/hooks/useClip/useClip.ts
+++ src/pages/DAW/components/Tracks/components/TrackView/components/ClipView/hooks/useClip/useClip.ts
@@ -3,7 +3,7 @@ import { useWaveSurfer } from "..";
 import { useAudioEngine } from "src/pages/DAW/hooks";
 import { useEffect, useMemo } from "react";
 import * as d3 from "d3";
-import { TOPBAR_HEIGHT } from "src/pages/DAW/constants";
+import { getDragHandler } from "./helpers";
 
 export const useClip = (clip: Clip) => {
   const audioEngine = useAudioEngine();
@@ -16,99 +16,7 @@ export const useClip = (clip: Clip) => {
 
   useEffect(() => {
     if (waveSurferRef.current) {
-      const currentParentTrackIndex = audioEngine.tracks.findIndex(
-        (track) => track.id === clip.track.id
-      );
-      const dragHandler = d3
-        .drag<any, unknown>()
-        .on("start", function () {
-          d3.select(this).raise();
-        })
-        .on("drag", function (event) {
-          audioEngine.moveSelectedClips(event.dx);
-          const indexDiff =
-            Math.round(
-              (event.y + TOPBAR_HEIGHT - currentParentTrackIndex * 84) / 84
-            ) - 1;
-
-          const newIndex = currentParentTrackIndex + indexDiff;
-
-          const inRange =
-            newIndex <= audioEngine.tracks.length - 1 && newIndex >= 0;
-
-          const trackToSelectIndex =
-            Math.round(
-              (event.y + TOPBAR_HEIGHT - currentParentTrackIndex * 84) / 84
-            ) - 1;
-
-          if (
-            audioEngine.selectedClips.every(
-              (selectedClip) => selectedClip.track.id === clip.track.id
-            ) &&
-            inRange
-          ) {
-            audioEngine.selectedClips.forEach((selectedClip) =>
-              selectedClip.setYOffset(trackToSelectIndex * 84)
-            );
-          }
-        })
-        .on("end", function (event) {
-          if (
-            audioEngine.selectedClips.every(
-              (selectedClip) => selectedClip.track.id === clip.track.id
-            )
-          ) {
-            audioEngine.selectedClips.forEach((selectedClip) => {
-              const indexDiff =
-                Math.round(
-                  (event.y + TOPBAR_HEIGHT - currentParentTrackIndex * 84) / 84
-                ) - 1;
-
-              const newIndex = currentParentTrackIndex + indexDiff;
-              const hasChangedTracks = newIndex !== currentParentTrackIndex;
-
-              const inRange =
-                newIndex <= audioEngine.tracks.length - 1 && newIndex >= 0;
-
-              if (hasChangedTracks) {
-                if (inRange) {
-                  audioEngine.tracks[newIndex]
-                    .addClip(
-                      selectedClip.audioBuffer,
-                      selectedClip.start.toSeconds(),
-                      selectedClip.fadeIn.toSamples(),
-                      selectedClip.fadeOut.toSamples()
-                    )
-                    .setSelect(true);
-                  selectedClip.deleteClip();
-                  audioEngine.getSelectedClips();
-                } else {
-                  const limitIndex =
-                    newIndex > audioEngine.tracks.length - 1
-                      ? audioEngine.tracks.length - 1
-                      : 0;
-                  audioEngine.tracks[limitIndex]
-                    .addClip(
-                      selectedClip.audioBuffer,
-                      selectedClip.start.toSeconds(),
-                      selectedClip.fadeIn.toSamples(),
-                      selectedClip.fadeOut.toSamples()
-                    )
-                    .setSelect(true);
-                  selectedClip.deleteClip();
-                  audioEngine.getSelectedClips();
-                }
-              }
-            });
-          }
-
-          if (audioEngine.timeline.snap) {
-            audioEngine.quantizeSelectedClips();
-          }
-          audioEngine.selectedClips.forEach((selectedClip) =>
-            selectedClip.setYOffset(0)
-          );
-        });
+      const dragHandler = getDragHandler({ audioEngine, clip });
 
       d3.select(waveSurferRef.current).call(dragHandler);
     }
